d0d4e3f18bfd6f72941e1661a0f4e74e15cf1545
dycoon <kuroda.dycoon@gmail.com>
2015-11-08 16:50:26 +0900

    modify: travis

--- libraries/cor_cocos2dx_converter/sources/textured_triangle_node.cpp
+++ libraries/cor_cocos2dx_converter/sources/textured_triangle_node.cpp
@@ -6,7 +6,7 @@
 #include "renderer/CCCustomCommand.h"
 #include "renderer/CCRenderer.h"
 #include "renderer/CCGLProgramState.h"
-#include "renderer/CCGLStateCache.h"
+#include "renderer/ccGLStateCache.h"
 #include "base/CCDirector.h"
 #include "base/CCEventListenerCustom.h"
 #include "base/CCEventDispatcher.h"
--- scripts/auto_release/release_android.rb
+++ scripts/auto_release/release_android.rb
@@ -7,19 +7,32 @@ $LOAD_PATH.push('../../libraries/scripts/lib')
 
 require 'cor/utility'
 
+@result = true
+def run_cmd(cmd)
+  puts "run -> #{cmd}"
+  r = system cmd
+  unless r
+    puts "faild on -> #{cmd}"
+  end
+  @result &= r
+  r
+end
+
 
 #
 Dir.chdir run_dir
 Dir.chdir '../init_work'
-system "ruby start_day_work.rb"
+run_cmd "ruby start_day_work.rb"
 
 #
 Dir.chdir run_dir
 Dir.chdir '../../projects/deploy_script'
 
-#system 'ruby deploy_android.rb ../majitai_running'
-system "ruby ../project_script/copy_project.rb ../majitai_running"
+#run_cmd 'ruby deploy_android.rb ../majitai_running'
+run_cmd "ruby ../project_script/copy_project.rb ../majitai_running"
 Dir.chdir "../cor_lib_test_main"
-system "cocos compile -p android -j 8 --ndk-mode release"
+run_cmd "cocos compile -p android -j 2 --ndk-mode release"
 
 puts "end of release_android.rb"
+
+exit @result
