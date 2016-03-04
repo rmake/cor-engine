67f52bca5714b5adcf054e5d686d26f48f0935b1
dycoon <kuroda.dycoon@gmail.com>
2016-03-04 23:52:59 +0900

    fix: test script argument.

--- tests/unit/proj.cmake/test.rb
+++ tests/unit/proj.cmake/test.rb
@@ -3,12 +3,19 @@ require "fileutils"
 FileUtils.chdir "#{File.dirname __FILE__}"
 @here = Dir::getwd
 
-# ruby test.rb -- --log_level=test_suite
-# ruby test.rb -- --run_test=each/each_vector
+# ruby test.rb --run-arg --log_level=test_suite
+# ruby test.rb --run-arg --run_test=each/each_vector
 
 args = ARGV
-build_args = args.slice_before do |v|
-  v == "--"
+build_args = []
+ct = 0
+args.each do |v|
+  build_args[ct] ||= []
+  if v == "--run-arg"
+    ct += 1
+  else
+    build_args[ct] << v
+  end
 end
 build_args = build_args.to_a
 
@@ -25,10 +32,10 @@ def run_cmd(cmd)
 end
 
 if RUBY_PLATFORM.downcase =~ /mswin(?!ce)|mingw|cygwin|bccwin/
-  run_cmd "ruby build_win64.rb #{build_args[0].join " "}"
-  run_cmd "build/win64/Debug/cor_test_main.exe #{build_args[1] ? build_args[1].join(" "): ""}"
+  run_cmd "ruby build_win64.rb #{build_args[0] ? build_args[0].join(" ") : ""}"
+  run_cmd "build/win64/Debug/cor_test_main.exe #{build_args[1] ? build_args[1].join(" ") : ""}"
 else
-  run_cmd "ruby build_default.rb #{build_args[0].join " "}"
-  run_cmd "build/default/cor_test_main #{build_args[1] ? build_args[1].join(" "): ""}"
+  run_cmd "ruby build_default.rb #{build_args[0] ? build_args[0].join(" ") : ""}"
+  run_cmd "build/default/cor_test_main #{build_args[1] ? build_args[1].join(" ") : ""}"
 end
 
