6b9a3712e70dc90b026cdf88aacecbc19bdf4a73
dycoon <kuroda.dycoon@gmail.com>
2016-03-21 13:43:53 +0900

    modify: update script.

--- scripts/init_work/pull_and_start_day_work.rb
+++ scripts/init_work/pull_and_start_day_work.rb
@@ -22,9 +22,17 @@ end
 source_branch = `git rev-parse --abbrev-ref HEAD`
 puts "source_branch = #{source_branch}"
 
-#run_cmd "git pull origin devel"
-run_cmd "git pull --rebase origin #{source_branch}"
+run_cmd `git fetch`
 
-run_cmd "ruby start_day_work.rb"
+is_ff = `git rev-list --count #{source_branch}..origin/#{source_branch}`
+puts "is_ff #{is_ff}"
+if is_ff == "0"
+  #run_cmd "git pull origin devel"
+  run_cmd "git pull --rebase origin #{source_branch}"
+
+  if @result
+    run_cmd "ruby start_day_work.rb"
+  end
+end
 
 exit @result
