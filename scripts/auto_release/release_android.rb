6003e40cda8dbcf33e1691405ddaba03cc4e3dd5
dycoon <kuroda.dycoon@gmail.com>
2016-03-21 11:26:48 +0900

    fix: release script

--- scripts/auto_release/release_android.rb
+++ scripts/auto_release/release_android.rb
@@ -25,22 +25,11 @@ Dir.chdir '../init_work'
 run_cmd "ruby start_day_work.rb"
 
 #
-#Dir.chdir run_dir
-#Dir.chdir '../../libraries/cor_all_cocos2dx/proj.cmake'
-#run_cmd "ruby make.rb android -f --for-ci --arch=armeabi-v7a release"
-
-#
 Dir.chdir run_dir
-#Dir.chdir '../../projects/deploy_script'
 
-Dir.chdir '../../projects/majitai_running'
+Dir.chdir '../../packages/examples/majitai_running'
 
 run_cmd "ruby make.rb android --for-ci --arch=armeabi-v7a release"
-
-#run_cmd 'ruby deploy_android.rb ../majitai_running'
-#run_cmd "ruby ../project_script/copy_project.rb ../majitai_running"
-#Dir.chdir "../cor_lib_test_main"
-#run_cmd "cocos compile -p android -j 2 --ndk-mode release --app-abi=armeabi-v7a"
 
 puts "end of release_android.rb"
 
